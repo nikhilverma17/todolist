3901f28d4033a8be69d57ab08b5dcfdece7859fc
Nikhil Verma <63592210+nikhilverma17@users.noreply.github.com>
2023-06-08 18:45:22 +0530

    Update date.js

--- date.js
+++ date.js
@@ -14,7 +14,8 @@ exports.getTime = function () {
     let d = new Date();
     let options = {
         hour: '2-digit',
-        minute: '2-digit'
+        minute: '2-digit',
+        timeZone: 'Asia/Kolkata'
 
     };
     return d.toLocaleTimeString("en-IN", options);
